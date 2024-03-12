9594a5856d35248cdaa4cffb3d918825fcedb487
unknown <117185466+NgokNgok04@users.noreply.github.com>
2024-03-12 14:48:43 +0700

    test

---
+++ kurvabejir/src/components/Header.js
@@ -0,0 +1,17 @@
+export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null)
+
+  function handleClick(){
+    setEnteredPlayerName(playerName.current.value)
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+      <p>
+        <input type="text" />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
