b5dc2ce76daba41e75a23b7edabe00997bb68c53
unknown <117185466+NgokNgok04@users.noreply.github.com>
2024-03-13 17:47:22 +0700

    test

--- kurvabejir/src/App.jsx
+++ kurvabejir/src/App.jsx
@@ -50,10 +50,6 @@ function App() {
     )
   }
 
-  const dataToJSON = () => {
-    const jsonData = JSON.stringify(arrayPoint);
-    console.log(jsonData);
-  }
   const handleInputChange = (index, value) => {
     let newValueX = parseFloat(value[0])
     let newValueY = parseFloat(value[1])
@@ -72,12 +68,10 @@ function App() {
 
     })
     setArrayPoint(Array(parseFloat(event.target.value)).fill([0,0]));
-    setShowChart(false);
   }
 
   function handleClick(){
     setShowChart((prevState) => !prevState)
-    dataToJSON()
   }
   return (
     <section id="player">
